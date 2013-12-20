22971c094a99f011be075ef6de9e8a9f237ef402
Albert Hung <ahung88@gmail.com>
2013-12-20 10:07:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyGoalsApp::Application.config.secret_token = '8a008902dae524830c1eded450464dfa4c4f767ec44e2bb44419bc40db39fd3ac1696a42fd6098e44ef4e4b39de0f1c5822fd2935110f12b33db2fdbc970e588'
